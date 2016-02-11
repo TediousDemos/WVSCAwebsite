1edd13be15651a4eb4bbb7cd7184c8cdc65ad172
TediousDemos <jackatimmons@gmail.com>
2016-02-10 20:09:54 -0500

    Trying to make everything MVC instead of putre HTML, this is a pure test

---
+++ homeWVSCA/homeWVSCA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="homeWVSCA.MvcApplication" Language="C#" %>
---
+++ homeWVSCA/homeWVSCA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(homeWVSCA.Startup))]
+namespace homeWVSCA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
